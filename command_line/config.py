2369d9d2a5025310a4e456e2884ad3433d9c8241
abyoussef <youssef949@hotmail.com>
2017-01-18 18:52:08 +0100

    add file

--- command_line/config.py
+++ command_line/config.py
@@ -3,4 +3,3 @@ data_dir = 'home/ubuntu/pynb/model/data/'  # directory to store algorithm data
 summary_dir = 'home/ubuntu/pynb/model/logs/'  # directory to store tensorflow summaries
 
 vocab_dir  = '/home/ubuntu/pynb/Data/flickr_data/text/vocab.txt' # directory of the 2000 most common tags. 
-
