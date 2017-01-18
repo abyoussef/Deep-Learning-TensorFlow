14468c494c31c1c763f8005d618c840537f20dba
Ubuntu <ubuntu@ip-172-31-21-182.us-west-2.compute.internal>
2017-01-18 17:45:58 +0000

    add image dir

--- command_line/config.py
+++ command_line/config.py
@@ -2,5 +2,8 @@ models_dir = 'home/ubuntu/pynb/model/stored_models/'  # dir to save/restore stor
 data_dir = 'home/ubuntu/pynb/model/data/'  # directory to store algorithm data
 summary_dir = 'home/ubuntu/pynb/model/logs/'  # directory to store tensorflow summaries
 
-vocab_dir  = '/home/ubuntu/pynb/Data/flickr_data/text/vocab.txt' # directory of the 2000 most common tags. 
+vocab_dir  = '/home/ubuntu/pynb/Data/flickr_data/text/' # directory of the 2000 most common tags. 
+img_dir = 'home/ubuntu/pynb/Data/flickr_data/image/' # directory of the image data 
+
+
 
