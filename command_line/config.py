80a8e101deae3b1375dc29ed45ed1145293326a4
abyoussef <youssef949@hotmail.com>
2017-01-22 15:26:48 +0100

    add notebook

--- command_line/config.py
+++ command_line/config.py
@@ -1,10 +1,10 @@
-models_dir = 'home/ubuntu/pynb/model/stored_models/'  # dir to save/restore stored_models
-data_dir = 'home/ubuntu/pynb/model/data/'  # directory to store algorithm data
-summary_dir = 'home/ubuntu/pynb/model/logs/'  # directory to store tensorflow summaries
+models_dir = 'model/stored_models/'  # dir to save/restore stored_models
+data_dir = 'model/data/'  # directory to store algorithm data
+summary_dir = 'model/logs/'  # directory to store tensorflow summaries
 
 vocab_dir  = '/home/ubuntu/pynb/Data/flickr_data/text/' # directory of the 2000 most common tags. 
-img_dir = 'home/ubuntu/pynb/Data/flickr_data/image/' # directory of the image data 
+flickr_unlabeled_path = '/home/ubuntu/pynb/Data/flickr_data/image/unlabelled' # directory of the image data unlabelled
+flickr_labeled_path = '/home/ubuntu/pynb/Data/flickr_data/image/labelled' # directory of the image data labelled
+
 
 img_vis = 'home/ubuntu/pynb/Data/images/' # directory for real images
-
-
